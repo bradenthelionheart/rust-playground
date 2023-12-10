b7dc452a7e30ea789045050075fb34d70cad1296
bradenthelionheart <93684663+bradenthelionheart@users.noreply.github.com>
2023-12-10 12:11:35 -0600

    Update hello.rs

--- src/hello.rs
+++ src/hello.rs
@@ -11,5 +11,6 @@ fn main() {
     // Statements here are executed when the compiled binary is called.
 
     // Print text to the console.
-    println!("Hello World!");
+    println!("Hello World!
+    I'm a Rustacean!");
 }
